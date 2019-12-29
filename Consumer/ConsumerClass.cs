495c61a94c5d41ff7c8486222e5190493b270b6c
fiyk <36037496+fiyk@users.noreply.github.com>
2019-12-29 23:35:29 +0300

    Add files via upload

---
+++ Consumer/ConsumerClass.cs
@@ -0,0 +1,37 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using Service;
+using System;
+using System.Text;
+
+namespace Consumer
+{
+    public class ConsumerClass
+    {
+        private readonly RabbitMQService _rabbitMQService;
+
+        public ConsumerClass(string queueName)
+        {
+            _rabbitMQService = new RabbitMQService();
+
+            using (var connection = _rabbitMQService.GetRabbitMQConnection())
+            {
+                using (var channel = connection.CreateModel())
+                {
+                    var consumer = new EventingBasicConsumer(channel);
+                    // Received event'i sürekli listen modunda olacak
+                    consumer.Received += (model, ea) =>
+                    {
+                        var body = ea.Body;
+                        var message = Encoding.UTF8.GetString(body);
+
+                        Console.WriteLine("{0} isimli queue üzerinden gelen mesaj: \"{1}\"", queueName, message);
+                    };
+
+                    channel.BasicConsume(queueName, true, consumer);
+                    Console.ReadLine();
+                }
+            }
+        }
+    }
+}
