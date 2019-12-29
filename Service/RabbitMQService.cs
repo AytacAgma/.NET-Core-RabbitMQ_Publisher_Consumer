582aee37818d429114911010561d50742208c487
fiyk <36037496+fiyk@users.noreply.github.com>
2019-12-29 23:02:07 +0300

    Add files via upload

---
+++ Service/RabbitMQService.cs
@@ -0,0 +1,26 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Service
+{
+    public class RabbitMQService
+    {
+        private readonly string _hostName = "localhost";
+        private readonly string _userName = "guest";
+        private readonly string _password = "guest";
+
+        public IConnection GetRabbitMQConnection()
+        {
+            ConnectionFactory connectionFactory = new ConnectionFactory()
+            {
+                HostName = _hostName,
+                UserName = _userName,
+                Password = _password
+            };
+
+            return connectionFactory.CreateConnection();
+        }
+    }
+}
