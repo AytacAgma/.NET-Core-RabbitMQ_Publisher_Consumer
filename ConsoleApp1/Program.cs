27f6390a5f9c7ac861c1514d17dd32149bb4fe62
fiyk <36037496+fiyk@users.noreply.github.com>
2019-12-29 23:38:09 +0300

    Add files via upload

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,22 @@
+﻿using Consumer;
+using Publisher;
+using System;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        private static readonly string _queueName = "aytacagma";
+        private static PublisherClass _publisher;
+        private static ConsumerClass _consumer;
+
+        static void Main(string[] args)
+        {
+            _publisher = new PublisherClass(_queueName, "RabbitMQ RabbitMQ");
+
+            _consumer = new ConsumerClass(_queueName);
+
+            Console.ReadKey();
+        }
+    }
+}
